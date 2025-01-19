33bfa8a7fd937237f63a8ddf844f4b813e2f428c
sanad <sanada.f15@gmail.com>
2025-01-19 18:28:57 +0200

    routing

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+
 const dirPath = path.join(__dirname, "/templates");
+const htmlFilePath = path.join(dirPath, "page.html");
+const cssFilePath = path.join(dirPath, "style.css");
 
-const file2Send = fs.readFileSync(`${dirPath}/index.html`);
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.end(file2Send);
+  if (req.url === "/style.css") {
+    res.setHeader("Content-Type", "text/css");
+    fs.createReadStream(cssFilePath).pipe(res);
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    fs.createReadStream(htmlFilePath).pipe(res);
+  }
 });
-
 server.listen(3005, () => {
   console.log("Server working on port 3000");
 });
